7942ce197b3d01f338cda9f2ae540d3a66c20f67
IsraelAdeniran <B00157067@mytudublin.ie>
2025-03-07 10:24:53 +0000

    Changes

--- DVS_Note/dvsnote-expo/App.js
+++ DVS_Note/dvsnote-expo/App.js
@@ -5,7 +5,7 @@ import { WebView } from 'react-native-webview';
 export default function App() {
   return (
       <WebView
-          source={{ uri: 'https://major-group-project-oi494visp-salems-projects-9acafb47.vercel.app/register' }}
+          source={{ uri: 'https://major-group-project-teal.vercel.app/register' }}
       />
   );
 }
