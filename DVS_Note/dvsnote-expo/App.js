bc3d57c3802da08f291453f6a51f7815a734a9c5
IsraelAdeniran <B00157067@mytudublin.ie>
2025-05-06 20:55:18 +0100

    Changes

--- DVS_Note/dvsnote-expo/App.js
+++ DVS_Note/dvsnote-expo/App.js
@@ -1,20 +1,17 @@
-import { StyleSheet } from 'react-native';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-      <WebView
-          source={{ uri: 'https://major-group-project-teal.vercel.app/register' }}
-      />
+      <View style={styles.container}>
+        <WebView source={{ uri: 'https://major-group-project-teal.vercel.app/register' }} />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
